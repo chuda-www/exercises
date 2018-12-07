c79eaed67d801c98e2efd9dd75562a9fe6b72570
Viktoryia Hrytsai <Viktoryia_Hrytsai@epam.com>
2018-12-07 14:39:29 +0300

    Deleted responseStatus OK

--- spring-web-app/src/main/java/com/epam/viktoryia/springdatabase/controller/RestController.java
+++ spring-web-app/src/main/java/com/epam/viktoryia/springdatabase/controller/RestController.java
@@ -7,7 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -25,7 +28,6 @@ public class RestController {
     }
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public List <Employee> getAllEmployee() {
         return createEmployeesService.getEmployees();
