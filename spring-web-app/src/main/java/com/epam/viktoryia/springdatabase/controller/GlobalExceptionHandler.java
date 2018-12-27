56b12c1ab847b69b0bf441ba595d9d9b776e511b
Viktoryia Hrytsai <Viktoryia_Hrytsai@epam.com>
2018-12-27 16:20:26 +0300

    Fixed import & GlobalExceptionHandler

--- spring-web-app/src/main/java/com/epam/viktoryia/springdatabase/controller/GlobalExceptionHandler.java
+++ spring-web-app/src/main/java/com/epam/viktoryia/springdatabase/controller/GlobalExceptionHandler.java
@@ -14,6 +14,6 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(DuplicateKeyException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String employeeDuplicate(DuplicateKeyException ex) {
-        return "DuplicateKeyException";
+        return ("*****DuplicateKeyException : " + ex.getMessage());
     }
 }
