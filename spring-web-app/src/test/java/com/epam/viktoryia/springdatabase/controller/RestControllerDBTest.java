565bd6846b7537db680871ec3424b2424ce30a55
Viktoryia Hrytsai <Viktoryia_Hrytsai@epam.com>
2019-01-08 17:33:04 +0300

    Fixed and adde jdbc

--- spring-web-app/src/test/java/com/epam/viktoryia/springdatabase/controller/RestControllerDBTest.java
+++ spring-web-app/src/test/java/com/epam/viktoryia/springdatabase/controller/RestControllerDBTest.java
@@ -1,19 +1,20 @@
 package com.epam.viktoryia.springdatabase.controller;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @RunWith(SpringRunner.class)
@@ -23,13 +24,15 @@ public class RestControllerDBTest {
     private MockMvc mockMvc;
     @Autowired
     private RestController restController;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Before
     public void init() throws Exception {
-        MockitoAnnotations.initMocks(this);
         mockMvc = MockMvcBuilders
                 .standaloneSetup(new GlobalExceptionHandler(), restController)
                 .build();
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, "Employee");
     }
 
     @Test
@@ -37,50 +40,58 @@ public class RestControllerDBTest {
 
         mockMvc.perform(MockMvcRequestBuilders.post("/employee")
                 .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
-                .content("[{\"name\": \"Joe\",\"age\": \"30\"},{\"name\": \"Joe\",\"age\": \"20\"}]"))
+                .content("[{\"name\": \"JJJ\",\"age\": \"30\"},{\"name\": \"JJJ\",\"age\": \"20\"}]"))
                 .andExpect(MockMvcResultMatchers.status()
                         .isBadRequest());
+        int rowsInTable1 = JdbcTestUtils.countRowsInTable(jdbcTemplate, "Employee");
+        Assert.assertEquals(0, rowsInTable1);
     }
 
     @Test
     public void createEmployeeTest() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.post("/employee")
                 .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
-                .content("[{\"name\": \"Joe\",\"age\": \"30\"}]"))
+                .content("[{\"name\": \"AAA\",\"age\": \"30\"}]"))
                 .andExpect(MockMvcResultMatchers.status()
                         .isCreated());
+        int rowsInTable1 = JdbcTestUtils.countRowsInTable(jdbcTemplate, "Employee");
+        Assert.assertEquals(1, rowsInTable1);
     }
 
     @Test
     public void getAllEmployeeTest() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.post("/employee")
-                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
-                .content("[{\"name\": \"Joe1\",\"age\": \"30\"}]"))
-                .andExpect(MockMvcResultMatchers.status()
-                        .isCreated());
+        String SQL = "INSERT INTO Employee (name, age) VALUES ('YYY', 30)";
+        jdbcTemplate.update(SQL);
+
         mockMvc.perform(MockMvcRequestBuilders.get("/employee"))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name", is("Joe1")))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].age", is(30)));
-
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("YYY"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].age").value(30));
+        int rowsInTable1 = JdbcTestUtils.countRowsInTable(jdbcTemplate, "Employee");
+        Assert.assertEquals(1, rowsInTable1);
     }
 
     @Test
     public void updateEmployeeTest() throws Exception {
-
+        String SQL = "INSERT INTO Employee (id, name, age) VALUES (1, 'VVV', 40)";
+        jdbcTemplate.update(SQL);
         mockMvc.perform(MockMvcRequestBuilders.put("/employee/1")
-                .content("\"Janett\"")
+                .content("\"XXX\"")
                 .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
                 .andExpect(MockMvcResultMatchers.status().isOk());
-
+        int rowsInTable1 = JdbcTestUtils.countRowsInTableWhere(jdbcTemplate, "Employee", "name = 'XXX'");
+        Assert.assertEquals(1, rowsInTable1);
     }
 
     @Test
     public void deleteEmployeeTest() throws Exception {
-
+        String SQL = "INSERT INTO Employee (id, name, age) VALUES (1, 'ABC', 40)";
+        jdbcTemplate.update(SQL);
         mockMvc.perform(MockMvcRequestBuilders.delete("/employee/1"))
                 .andExpect(MockMvcResultMatchers.status().isOk());
+        int rowsInTable1 = JdbcTestUtils.countRowsInTable(jdbcTemplate, "Employee");
+        Assert.assertEquals(0, rowsInTable1);
     }
 }
 
