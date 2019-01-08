3ac332fd2b2b7541dac4d86bdf4749714fd86baa
Viktoryia Hrytsai <Viktoryia_Hrytsai@epam.com>
2019-01-08 16:05:20 +0300

    fixed

--- spring-web-app/src/test/java/com/epam/viktoryia/springdatabase/controller/GlobalExceptionHandlerDBTest.java
+++ spring-web-app/src/test/java/com/epam/viktoryia/springdatabase/controller/GlobalExceptionHandlerDBTest.java
@@ -1,13 +1,10 @@
 package com.epam.viktoryia.springdatabase.controller;
 
-import com.epam.viktoryia.springdatabase.service.EmployeesService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -16,16 +13,11 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.mockito.Matchers.anyList;
-import static org.mockito.Mockito.doThrow;
-
 @RunWith(SpringRunner.class)
 @ContextConfiguration("/spring-context.xml")
-public class GlobalExceptionHandlerTestDB {
+public class GlobalExceptionHandlerDBTest {
 
     private MockMvc mockMvc;
-    @Mock
-    private EmployeesService employeesService;
     @Autowired
     private RestController restController;
 
@@ -33,14 +25,12 @@ public class GlobalExceptionHandlerTestDB {
     public void init() throws Exception {
         MockitoAnnotations.initMocks(this);
         mockMvc = MockMvcBuilders
-                .standaloneSetup(new GlobalExceptionHandler(),restController)
+                .standaloneSetup(new GlobalExceptionHandler(), restController)
                 .build();
     }
 
     @Test
     public void employeeDuplicate() throws Exception {
-
-        doThrow(new DuplicateKeyException("abc")).when(employeesService).createEmployees(anyList());
 
         mockMvc.perform(MockMvcRequestBuilders.post("/employee")
                 .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
